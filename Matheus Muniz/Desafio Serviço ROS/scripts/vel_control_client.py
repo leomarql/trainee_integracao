9256cefd2b297bfbf3689b449d08be39236d887a
M-Muniz <133266092+M-Muniz@users.noreply.github.com>
2023-05-30 21:11:50 -0300

    Desafio ROS Service

---
+++ Matheus Muniz/Desafio Serviço ROS/scripts/vel_control_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import rospy
+from beginner_tutorials.srv import *
+
+def vel_control_client(x, y):
+    rospy.wait_for_service('vel_control')
+    try:
+        vel_control = rospy.ServiceProxy('vel_control', Vel_Control)
+        resp1 = vel_control(x, y)
+        return resp1.resultado
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:
+        print(usage())
+        sys.exit(1)
+    print("Posição x:%s, y:%s" % (x, y))
+    response = vel_control_client(x, y)
+    print("Resposta do serviço: %s" % response)
+
